a0314c98c1f8b171013f0cb8092b7739329b28fb
andreidanilenko <andrei.danilenko@gmail.com>
2024-08-14 23:36:42 +0300

    Add files via upload

--- 'module_1_3.py'.py
+++ 'module_1_3.py'.py
@@ -2,12 +2,12 @@ name="Andrei"
 print(name)
 age=60
 print(age)
-new_age= age+10
-print(new_age)
+age= age+10
+print(age)
 is_student=True
 print(is_student)
-print('Name:',name,'Age:',age,'New age:',new_age, 'Is Student:',is_student)
+print('Name:',name,'Age:',age-10,'New age:',age, 'Is Student:',is_student)
 print('Name:',name,)
-print('Age:',age,)
-print('New age:',new_age)
+print('Age:',age-10,)
+print('New age:',age)
 print('Is Student:',is_student)
