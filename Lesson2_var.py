f314dac2927a481db9aa910ed593cd55bde33b99
andreidanilenko <andrei.danilenko@gmail.com>
2024-08-14 18:40:10 +0300

    Add files via upload

---
+++ Lesson2_var.py
@@ -0,0 +1,5 @@
+Homework_task_done=12
+Manpower=1.5
+Name_of_Course='Python'
+mean_homework_task_time=(Manpower/Homework_task_done)
+print('Курс:',Name_of_Course,', всего задач:',Homework_task_done,', затрачено часов:',Manpower,', среднее время выполнения:',mean_homework_task_time, 'часа.',sep="")
