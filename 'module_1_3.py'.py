6771900b3054be84bc31f5c64797cac50d715632
andreidanilenko <andrei.danilenko@gmail.com>
2024-08-14 18:37:32 +0300

    Add files via upload

---
+++ 'module_1_3.py'.py
@@ -0,0 +1,13 @@
+name="Andrei"
+print(name)
+age=60
+print(age)
+new_age= age+10
+print(new_age)
+is_student=True
+print(is_student)
+print('Name:',name,'Age:',age,'New age:',new_age, 'Is Student:',is_student)
+print('Name:',name,)
+print('Age:',age,)
+print('New age:',new_age)
+print('Is Student:',is_student)
