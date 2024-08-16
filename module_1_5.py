92ed43eb98617b47039da66cc403921c2127e8f9
andreidanilenko <andrei.danilenko@gmail.com>
2024-08-16 23:37:12 +0300

    Выполнено домашнее задание по модулю_1_5.
    Signed-off-by: andreidanilenko <andrei.danilenko@gmail.com>

---
+++ module_1_5.py
@@ -0,0 +1,57 @@
+immutable_var=tuple_=2024,True,'module'
+print(immutable_var)
+print('Immutable tuple: ',immutable_var)
+mutable_list=[2024,True,'module']
+mutable_list.append('Modified')
+print('Mutable list: ',(mutable_list))
+
+
+# food=['apple','coconut','banana']
+# print(food)
+# print(food[0])
+# food[0]='peach'
+# print(food)
+# food[1]='apricot'
+# print(food)
+# food.append(True)
+# print(food)
+# food.extend('string')
+# print(food)
+# food.extend(["string",2])
+# print(food)
+# food.remove("apricot")
+# print(food)
+# print('coconut' in food)
+# print('coconut' not in food)
+# print(food[0:3])
+# print(food[0:3:2])
+
+# tuple_=1,2,3,4
+# tuple_2=(1,2,3,4)
+# tuple_3=tuple([1,2,3,4])
+# print(tuple_)
+# print(tuple_2)
+# print(tuple_3)
+# print(type(tuple_))
+
+# tuple_=1,2,True,'String'
+# tuple_2=(1,2,3,4)
+# tuple_3=tuple([1,2,3,4])
+# print(tuple_)
+# print(tuple_2)
+# print(tuple_3)
+# print(type(tuple_))
+# print(tuple_[0])
+# ##! Ошибка! tuple_[0]=200
+# list_=[1,2,True,'String']
+# print(tuple_.__sizeof__())
+# print(list_.__sizeof__())
+# tuple_=([1,2],0)+(1,2)
+# print(tuple_)
+# tuple_[0][1]=3
+# print(tuple_)
+# tuple_=(1,2)*3
+# print(tuple_)
+
+
+
