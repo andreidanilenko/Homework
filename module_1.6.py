8804988bc8ce9f987b148a31cd2100b41d33198f
andreidanilenko <andrei.danilenko@gmail.com>
2024-08-22 14:50:53 +0300

    module_1_6_ is done & added lesson task
    Signed-off-by: andreidanilenko <andrei.danilenko@gmail.com>

--- module_1.6.py
+++ module_1.6.py
@@ -14,3 +14,47 @@ my_set.add(5)
 my_set.add('Kamila')
 my_set.remove(1)
 print('Modified set: ',my_set)
+
+# phone_book={'Denis': 88005553535, 'Max': 88005553534}
+# print(phone_book)
+# #Ошибка если Денис в квадратные скобки phone_book={['Denis']: 88005553535, 'Max': 88005553534}
+# print(phone_book['Denis'])
+# phone_book['Denis']=1513489346
+# print(phone_book)
+# print(phone_book['Denis'])
+# phone_book['Anton']=88005553533
+# print(phone_book)
+# del(phone_book['Max'])
+# print(phone_book)
+# phone_book.update({'Sasha':88005553532,'Alex': 88005553531})
+# print(phone_book)
+# print (phone_book.get ('Denis'))
+# print (phone_book.get ('Kamila'))
+# print (phone_book.get ('Kamila', 'Такого ключа нет'))
+# a=phone_book.pop('Anton')
+# print(a)
+# print(phone_book)
+# list_=[1,2,3]
+# list_.pop(0)
+# print(list_)
+# print(phone_book)
+# print(phone_book.keys())
+# print(phone_book.values())
+# print(phone_book.items())
+# phone_book={'Denis': [88005553535, 204324], 'Max': 88005553534}
+# print(phone_book)
+# set_={1,2,3,4,5,1,2,3,4,'String',False,(1,2,3)}# Логическое значение в множестве не выводится на печать, если есть символы "1"=True или "0"=False
+# print(set_)
+# list_=[1,1,1,2,2,2,3,4]
+# print(set(list_))
+# list_=set(list_)
+# print(list_) # print(list_[0]) ,извлекать по индексу будет ошибка, так как это множество
+# print(list_.discard(1))
+# print(list_)
+# print(list_.remove(3))
+# print(list_)
+# list_.add(5)
+# print(list_)
+#
+# name=input('Введите имя: ')
+# print (phone_book.get (name, 'Такого ключа нет'))
