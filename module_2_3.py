2bdd90db8447d35e31296ed93f214b4653bc8e66
andreidanilenko <andrei.danilenko@gmail.com>
2024-08-23 11:05:23 +0300

    module_2_3_ is done with teacher
    Signed-off-by: andreidanilenko <andrei.danilenko@gmail.com>

---
+++ module_2_3.py
@@ -0,0 +1,14 @@
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+
+count=0
+while count < len(my_list):
+
+    if my_list[count] == 0:
+        count = count + 1
+        continue
+
+    if my_list[count]<0:
+        break
+
+    print(count, my_list[count])
+    count=count+1
