444fb1d61d2c9d3813ceafd1f5b93949e9e747c6
andreidanilenko <andrei.danilenko@gmail.com>
2024-08-25 08:33:06 +0300

    module_2_3_ is done with teacher
    Signed-off-by: andreidanilenko <andrei.danilenko@gmail.com>

---
+++ vebinar module_2.py
@@ -0,0 +1,23 @@
+# password= ""
+# name= ""
+#
+# while password!= "12345" or name != 'Андрей'
+
+# fruit_list=["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+#     if fruit == 'cherry'
+#         continue
+#         break
+# for fruit in "fruit_list":
+
+#     print(fruit)
+
+ #   for i in range(10):
+
+def line(length, height):
+    for i in range ():
+     print("-"* length)
+line()
+
+print("________________")
+print("Hello, Gamer")
+line(4)
