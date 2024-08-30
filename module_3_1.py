55e98289051e9069f7222d96474d05108116cf60
andreidanilenko <andrei.danilenko@gmail.com>
2024-08-30 07:36:48 +0300

    module_3_1_ is done_with home task
    Signed-off-by: andreidanilenko <andrei.danilenko@gmail.com>

--- module_3_1.py
+++ module_3_1.py
@@ -1,10 +1,56 @@
-import string
+calls = 0   # 1
+def count_calls():  #2
+    global calls
+    calls += 1
 
-calls = 0
-count_calls: int = calls
-string_info: ('string')
-is_contains: (string, 'list_to_search')
+def string_info(string):    #3
+    stroka = str(string)
+    result = (len(stroka), stroka.upper(), stroka.lower())
+    count_calls()
+    return result
 
+def is_contains(string, list_to_search):    # 4
+    string = str(string).lower()
+    list_to_search = list(list_to_search)
+    count_calls()
+    for i in range(len(list_to_search)):
+        if str(list_to_search[i]).lower() == string:
+            result = True
+            break
+        else:
+            result = False
+            continue
+    return result
+
+print(string_info('Capybara'))
+print(string_info('Armageddon'))
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN'])) # Urban ~ urBAN
+print(is_contains('cycle', ['recycling', 'cyclic'])) # No matches
+print(calls)
+
+
+# import string
+#
+# calls = 0
+# count_calls: int = calls
+# string_info: ('string')
+# is_contains: (string, 'list_to_search')
+
+# Функция count_calls подсчитывающая вызовы остальных функций.
+# Функция string_info принимает аргумент - строку и возвращает кортеж из: длины этой строки, строку в верхнем регистре, строку в нижнем регистре.
+# Функция is_contains принимает два аргумента: строку и список, и возвращает True, если строка находится в этом списке, False - если отсутствует. Регистром строки при проверке пренебречь: UrbaN ~ URBAN.
+
+# Пункты задачи:
+# Создать переменную calls = 0 вне функций.
+# Создать функцию count_calls и изменять в ней значение переменной calls. Эта функция должна вызываться в остальных двух функциях.
+# Создать функцию string_info с параметром string и реализовать логику работы по описанию.
+# Создать функцию is_contains с двумя параметрами string и list_to_search, реализовать логику работы по описанию.
+# Вызвать соответствующие функции string_info и is_contains произвольное кол-во раз с произвольными данными.
+# Вывести значение переменной calls на экран(в консоль).
+
+# Примечания:
+# Для использования глобальной переменной внутри функции используйте оператор global.
+# Для функции is_contains лучше привести и искомую строку и все строки в списке в один регистр.
 
 
 # Пример выполняемого кода:
@@ -20,3 +66,24 @@ is_contains: (string, 'list_to_search')
 # True
 # False
 # 4
+
+
+
+# a = 5
+# b = 10
+#
+#
+# def printer():
+#     global a, b
+#     a = 'Str'
+#     b = 'Str 2'
+#
+#     c = 15
+#     d = 20
+#     print(c, d, 'local')
+#     print(a, b, 'global')
+#
+#
+# print(a, b)
+# printer()
+# print(a, b)
