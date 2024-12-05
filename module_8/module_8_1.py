02b88180d1820f72b25ab903ec0a9f91643bcfaa
andreidanilenko <andrei.danilenko@gmail.com>
2024-12-05 20:15:12 +0300

    Задача "Try и Except":
    Signed-off-by: andreidanilenko <andrei.danilenko@gmail.com>

--- module_8/module_8_1.py
+++ module_8/module_8_1.py
@@ -1,12 +1,13 @@
-try:
-    def add_everything_up(a, b):
-        print(a + b)
+def add_everything_up(a, b):
+    try:
+        return a + b
 
-except TypeError:
+    except TypeError:      # Если возникает ошибка, возвращаем строковое представление двух данных
+        return str(a) + str(b)  # Пытаемся сложить a и b
 
-    print(add_everything_up(123.456, 'строка'))
-    print(add_everything_up('яблоко', 4215))
-    print(add_everything_up(123.456, 7))
+print(add_everything_up(123.456, 'строка'))
+print(add_everything_up('яблоко', 4215))
+print(add_everything_up(123.456, 7))
 
 
 
